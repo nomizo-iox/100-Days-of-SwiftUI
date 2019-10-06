997dcf28ebb105172c1f5ffc840ccd95b0a39df3
nomizo-iox <nomizo.iox@gmail.com>
2019-10-05 21:04:04 -0500

    second commit.

---
+++ Day1.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+var str = "Hello, playground"
+
+str = "Goodbye"
+
+// Interger or Int
+var population = 3_000_000
+
+// Multiline Quote
+var str1 = """
+This is a multiline string,
+what do you have to say about that?
+"""
+
+// Double
+var pi = 3.14
+
+// Bool
+var awesome = true
+
+
+// String Interpolation
+var name = "Samuel"
+var results = "Your name is \(name)"
+
+// Constants
+let birthday = "July 23, 1987"
+
+
+// Type Annotations 
+let age: Int = 30
+let album: String = "Bad"
+let height: Double = 1.78
+let BobbyIsSick: Bool = true
